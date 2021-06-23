110488223f5e1fd0b7aa0864e06e243701317eac
beneko <behzadnekouei@gmail.com>
2021-06-23 11:23:54 +0200

    set password hashing

--- src/Form/UtilisateursType.php
+++ src/Form/UtilisateursType.php
@@ -4,7 +4,11 @@ namespace App\Form;
 
 use App\Entity\Utilisateurs;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,10 +17,14 @@ class UtilisateursType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('mail')
-            ->add('password')
-            ->add('nom')
-            ->add('prenom')
+            ->add('nom', TextType::class)
+            ->add('prenom', TextType::class)
+            ->add('mail', EmailType::class)
+            ->add('password', RepeatedType::class, array(
+                'type' => PasswordType::class,
+                'first_options'  => array('label' => 'Mot de passe'),
+                'second_options' => array('label' => 'Confirme mot de passe'),
+            ))
         ;
     }
 
